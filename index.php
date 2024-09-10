e5988e8decb269aef2db5713ea06fdd7671e57d3
David <dbarrosoc2@gmail.com>
2024-09-10 20:30:58 +0200

    XXX

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>David Alejandro Barroso Contreras - Portafolio</title>
+    <title>- David Alejandro Barroso Contreras - Portafolio - </title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
