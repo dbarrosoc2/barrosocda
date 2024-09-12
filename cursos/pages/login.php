5860c2fd165c57fcd065d38c4ef18e750ec5f27b
David <dbarrosoc2@gmail.com>
2024-09-12 13:02:37 +0200

    XXxX

--- cursos/pages/login.php
+++ cursos/pages/login.php
@@ -6,8 +6,6 @@ session_start();
 <?php
 $title = "LOGIN";
 require_once '../common/head.php';
-require_once './Logs.php'; //clase para gestionar la trazabilidad 
-$logs = new Logger();
 $ocultarFormulario = "block";
 ?>
 
@@ -17,135 +15,88 @@ $ocultarFormulario = "block";
         $mensaje = '<div class="alert alert-danger text-center" role="alert">';
         $mensaje .= 'Usuario y/o contraseña incorrectos';
         $mensaje .= '</div>';
-        if (session_status() == PHP_SESSION_ACTIVE) {
-            session_destroy();
-        }
-        echo $mensaje;
+        unset($_SESSION['errorDatos']); // Only unset errorDatos, don't destroy the whole session
     }
+
     $errores = array();
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-        function limpiar($variable)
-        {
-            $variable = trim(htmlspecialchars(stripslashes($variable)));
-            return $variable;
+        function limpiar($variable) {
+            return trim(htmlspecialchars(stripslashes($variable)));
         }
-        $user = isset($_POST['user']) ? limpiar($_POST['user']) : " ";
-        $pass = isset($_POST['pass']) ? limpiar($_POST['pass']) : " ";
-        $hora = isset($_POST['hora_login']) ? limpiar($_POST['hora_login']) : " ";
+
+        $user = isset($_POST['user']) ? limpiar($_POST['user']) : "";
+        $pass = isset($_POST['pass']) ? limpiar($_POST['pass']) : "";
+        $hora = isset($_POST['hora_login']) ? limpiar($_POST['hora_login']) : "";
 
         if (empty($user)) {
-            $errores['a'] = '<div class="alert alert-danger text-center" role="alert">';
-            $errores['a'] .= 'Ingresa por favor un usuario';
-            $errores['a'] .= '</div>';
+            $errores['a'] = '<div class="alert alert-danger text-center" role="alert">Ingresa por favor un usuario</div>';
         }
         if (empty($pass)) {
-            $errores['b'] = '<div class="alert alert-danger text-center" role="alert">';
-            $errores['b'] .= 'Por favor ingresa tu contraseña';
-            $errores['b'] .= '</div>';
-
+            $errores['b'] = '<div class="alert alert-danger text-center" role="alert">Por favor ingresa tu contraseña</div>';
         }
         if (empty($hora)) {
-            $errores['c'] = '<div class="alert alert-danger text-center" role="alert">';
-            $errores['c'] .= '****Error en el envio del formulario****';
-            $errores['c'] .= '</div>';
-
+            $errores['c'] = '<div class="alert alert-danger text-center" role="alert">****Error en el envio del formulario****</div>';
         }
-
         if (empty($errores)) {
-            $mensaje = '';
-            if (isset($_POST['submit']) && !empty($_POST['user']) && !empty($_POST['pass'])) {
-                $site = "10.35.50.32";
-                $port = 6544;
-                set_time_limit(0);
-                $fp = fsockopen($site, $port, $errno, $errstr, 10);
-                if (!$fp) {
-                    echo "$errstr ($errno)\n";
-                    $logs->login($user, $pass, $_SERVER['REMOTE_ADDR'], "Error o en usuario o clave1");
+            try {
+                // Requiere la conexión a la base de datos
+                require 'conexion.php';
+
+                // Consulta para verificar el usuario
+                $consulta = $pdo->prepare("SELECT dni, cod_empleado, nombre, apellidos, email, telefono, password, ultima_conexion, rol 
+                                           FROM personal 
+                                           WHERE dni = :dni AND password = :password");
+                $consulta->bindParam(':dni', $user); // Assuming $user is the DNI
+                $consulta->bindParam(':password', $pass); // Assuming $pass is the plain password
+                $consulta->execute();
+                $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+
+                if ($resultado) { // If a row is returned, the user exists and password matches
+                    // Redirige a loginOk por que todo va OK...!
+                    $_SESSION['valid'] = true;
+                    $_SESSION['timeout'] = $hora;
+                    $_SESSION['usuario'] = $resultado['dni'];
+                    $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+                    $_SESSION['nombre'] = $resultado['nombre'];
+                    $_SESSION['apellidos'] = $resultado['apellidos'];
+                    $_SESSION['telefono'] = $resultado['telefono'];
+                    $_SESSION['email'] = $resultado['email'];
+                    $_SESSION['idempleado'] = $resultado['cod_empleado'];
+                    $_SESSION['rol'] = $resultado['rol'];
+
+                    header("Location: inicio.php");
+                    exit();
                 } else {
-                    //log ok 
-                    $myObj = new stdClass();
-                    $myObj->user = $user;
-                    $myObj->password = $pass;
-                    $myJSON = json_encode($myObj);
-                    $a = fwrite($fp, $myJSON);
-                    while (!feof($fp)) {
-                        //echo $a;
-                        $output = fgets($fp, 2048);
-                    }
-                    $obj = json_decode($output);
-
-                    $_SESSION['a'] = $obj;
-
-                }
-            }
-            if ($obj->error == null && $obj->rol != 'Sin permisos') {
-                $logs->login($user, $pass, $_SERVER['REMOTE_ADDR'], "Login Correcto");
-                $_SESSION['valid'] = true;
-                $_SESSION['timeout'] = $hora;
-                $_SESSION['usuario'] = $obj->usuario;
-                //$_SESSION['rol'] = $obj->rol;
-                $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-                $_SESSION['nombre'] = $obj->nombre;
-                $_SESSION['apellidos'] = $obj->apellidos;
-                $_SESSION['telefono'] = $obj->telefono;
-                $_SESSION['email'] = $obj->email;
-                $_SESSION['idempleado'] = $obj->idempleado;
-                $_SESSION['pass'] = $_POST['pass'];
-                // var_dump($_SESSION);
-                //header('Refresh: 0; URL = login.php');
-                if (isset($_SESSION['valid'])) {
-                    // var_dump($output);
-                    // var_dump($obj);
-                    // var_dump($a);
-                    header("Location: validacionBBDDinterna.php?password=" . urlencode($pass));
+                    $_SESSION['errorDatos'] = true;
+                    header("Location: " . $_SERVER['PHP_SELF']);
                     exit();
                 }
-            } elseif ($obj->error == null && $obj->rol == 'Sin permisos') {
-                $logs->login($user, $pass, $_SERVER['REMOTE_ADDR'], "Usuario sin permisos");
-                $mensaje = '<div class="alert alert-danger text-center" role="alert">';
-                $mensaje .= 'Usuario sin Permisos';
-                $mensaje .= '</div>';
-                echo $mensaje;
-            } else {
-                $logs->login($user, $pass, $_SERVER['REMOTE_ADDR'], "Datos no validos");
-                $mensaje = '<div class="alert alert-danger text-center" role="alert">';
-                $mensaje .= 'Los datos suministrados no son correctos';
-                $mensaje .= '</div>';
-                echo $mensaje;
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
             }
-        } else {
-            $logs->login($user, $pass, $_SERVER['REMOTE_ADDR'], "Error o en usuario o clave");
-            $mensaje = '<div class="alert alert-danger text-center" role="alert">';
-            $mensaje .= 'Debes ingresar tu nombre de usuario y la contraseña';
-            $mensaje .= '</div>';
-            echo $mensaje;
         }
-    } else {
-        echo "<div class='alert alert-danger'>";
-        echo implode("<br>", $errores);
-        echo "</div>";
     }
-
     ?>
     <div class="container mt-3">
         <div class="container-fluid h-100">
             <div class="row align-items-center justify-content-center h-100">
-                <div class="col-md-6 text-center bg-white p-4 rounded">
+                <div class="col-md-6 col-lg-4 text-center bg-white p-4 rounded shadow-sm">
                     <div class="d-flex justify-content-center align-items-center mb-4">
                         <img src="../public/logo.jpg" alt="logoHospital" class="img-fluid">
                     </div>
                     <div class="card">
                         <div class="card-body" style="display: <?= $ocultarFormulario; ?>">
                             <h1 class="text-center mb-4">
-                                <span class="d-block"
-                                    style="font-size: 1.5rem; font-family: 'YourFont', sans-serif;">Gestión de Cursos y
-                                    Certificados</span>
-                                <span class="d-block"
-                                    style="font-size: 1rem; font-family: 'YourFont', sans-serif;">Inicio de
-                                    sesión</span>
+                                <span class="d-block" style="font-size: 1.5rem; font-family: 'YourFont', sans-serif;">Gestión de Cursos y Certificados</span>
+                                <span class="d-block" style="font-size: 1rem; font-family: 'YourFont', sans-serif;">Inicio de sesión</span>
                             </h1>
-
-                            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                            <?php
+                            if (isset($mensaje)) {
+                                echo $mensaje;
+                            }
+                            ?>
+                            <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
                                 <div class="mb-3">
                                     <label for="user" class="form-label">Usuario:</label>
                                     <input type="text" class="form-control" name="user" id="user">
@@ -154,27 +105,21 @@ $ocultarFormulario = "block";
                                     <label for="pass" class="form-label">Contraseña:</label>
                                     <input type="password" class="form-control" name="pass" id="pass">
                                 </div>
-                                <input type="hidden" name="hora_login" id="hora_login"
-                                    value="<?php echo date('d-m-Y H:i:s'); ?>">
-                                <button type="submit" class="btn btn-primary" name="submit">Acceder</button>
+                                <input type="hidden" name="hora_login" id="hora_login" value="<?= date('d-m-Y H:i:s'); ?>">
+                                <button type="submit" class="btn btn-primary w-100" name="submit">Acceder</button>
                             </form>
+                            <a href="#" class="d-block mt-3">¿Olvidaste tu contraseña?</a>
                         </div>
                         <div class="mt-1">
-                            <a href="#" class="text-decoration-none">
-                                <br><br><br><br>
-                            </a>
+                            <a href="#" class="d-block">Regístrate</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-    </div>
-    <br><br><br>
     <?php
     require __DIR__ . '/../common/footer.php';
     ?>
 </body>
-
 </html>
