040bbcf991fd1b82b535c70c44290a9660197e3f
David <dbarrosoc2@gmail.com>
2024-09-18 21:01:45 +0200

    AFERESIS

--- aferesis/startbootstrap-sb-admin-gh-pages/aside.php
+++ aferesis/startbootstrap-sb-admin-gh-pages/aside.php
@@ -1,7 +1,7 @@
 <?php
-//if (session_status() === PHP_SESSION_NONE) {
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
-//}
+}
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
