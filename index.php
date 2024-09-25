994e015dac54c0071f9906afacdae9b9334e3773
David <dbarrosoc2@gmail.com>
2024-09-25 13:46:01 +0200

    fondo

--- index.php
+++ index.php
@@ -14,7 +14,9 @@
         body {
             /* background-color: #f8f9fa; */
             font-family: 'Poppins', sans-serif;
-            background:no-repeat url(th.jpeg);
+            background: no-repeat center center url('th.jpeg');
+            background-size: cover;
+            background-attachment: fixed;
         }
 
         h1 {
