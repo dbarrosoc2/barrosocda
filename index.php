812aa7706dd3bf901d7954c1cc61b9ed75c86aa0
David <dbarrosoc2@gmail.com>
2024-09-25 14:00:56 +0200

    fondo

--- index.php
+++ index.php
@@ -235,7 +235,7 @@ p {
         </div>
     </div>
     <a href="#" class="logo-button">
-    <img src="DavidBarroso.png" alt="Logo" class="logo-icon">
+    <img src="David_Barroso2.png" alt="Logo" class="logo-icon">
 </a>
 
 
