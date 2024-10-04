5a0a784db7d35f6fccb9790b21590f9c8313ceed
David <dbarrosoc2@gmail.com>
2024-10-04 13:43:23 +0200

    fondo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>David Barroso - Portafolio</title>
+    <title>David Barroso - Portafolio ++++++++++</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
